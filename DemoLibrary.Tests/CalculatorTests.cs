f2cc00e91d639853429cf91872576ac8b37bf9f9
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-03-28 22:18:20 +0300

    Added xUnit test project and first theory-test

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(2, 4, 6)]
+        [InlineData(-1, -3, -4)]
+        [InlineData(10, 2.34, 12.34)]
+        [InlineData(double.MaxValue, 5, double.MaxValue)]
+        public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = Calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
