f6f65059c1ab330ff63ec69078c8af675aabd3e3
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-03-28 22:58:58 +0300

    Testing more methods that throw exceptions.

---
+++ DemoLibrary.Tests/ExampleMethodsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class ExampleMethodsTests
+    {
+        [Fact]
+        public void LoadTextFile_ValidNameShouldWork()
+        {
+            // Act
+            string actual = ExampleMethods.LoadTextFile("this is a valid file path");
+
+            // Assert
+            Assert.True(actual.Length >= 10);
+        }
+
+        [Fact]
+        public void LoadTextFile_InvalidNameShouldThrowException()
+        {
+            // Assert
+            Assert.Throws<FileNotFoundException>(() => ExampleMethods.LoadTextFile("bla"));
+        }
+    }
+}
---
+++ DemoLibrary/ExampleMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace DemoLibrary
+{
+    public static class ExampleMethods
+    {
+        public static string LoadTextFile(string filePath)
+        {
+            if (filePath.Length < 10)
+            {
+                throw new FileNotFoundException();
+            }
+
+            return "The file was correctly loaded.";
+        }
+    }
+}
